6aaeff22cc44b1718b1e13b287ba2a88b348232b
wangyihan <572466520@qq.com>
2017-10-13 11:46:43 +0800

    注释

---
+++ WyhSDK/WyhSDKHeaderTest.h
@@ -0,0 +1,26 @@
+//
+//  WyhSDKHeaderTest.h
+//  WyhSDK
+//
+//  Created by 王乙涵 on 2017/10/13.
+//  Copyright © 2017年 王乙涵. All rights reserved.
+//
+/*!头文件基本信息。这个用在每个源代码文件的头文件的最开头。
+ 
+ @headerWyhSDKHeaderTest.h
+ 
+ @abstract 关于这个源代码文件的一些基本描述
+ 
+ @author Created by sindri Lin on2017/10/13.
+ 
+ @version1.002017/10/13Creation(此文档的版本信息)
+ 
+ Copyright © 2017年 王乙涵. All rights reserved.
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface WyhSDKHeaderTest : NSObject
+
+@end
---
+++ WyhSDK/WyhSDKHeaderTest.m
@@ -0,0 +1,13 @@
+//
+//  WyhSDKHeaderTest.m
+//  WyhSDK
+//
+//  Created by 王乙涵 on 2017/10/13.
+//  Copyright © 2017年 王乙涵. All rights reserved.
+//
+
+#import "WyhSDKHeaderTest.h"
+
+@implementation WyhSDKHeaderTest
+
+@end
